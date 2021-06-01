05d6fbc34f283f6f0a4ad0933b04f94ffed8dd3e
DUVALFrancois503 <francois.duval.503@gmail.com>
2021-06-01 09:58:46 +0200

    Création Sortie Affichage Liste Sortie Affichage Détail Sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('dateDebut')
+            ->add('duree')
+            ->add('dateFin')
+            ->add('nbInscriptionsMax')
+            ->add('description')
+            ->add('urlPhoto')
+            ->add('user')
+            ->add('etat')
+            ->add('lieu')
+            ->add('campus')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
